a25ab91bea079dacc6d80b5b75757abd2fbf7375
ArtBondar <mr.pika.2017@gmail.com>
2022-10-14 12:50:36 +0300

    Create random in lab02

---
+++ src/main/java/ua/khpi/oop/bondar02/Main.java
@@ -0,0 +1,11 @@
+package ua.khpi.oop.bondar02;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String args[]) {
+        int rand = new Random().nextInt(999999);
+        System.out.println("Random number: " + rand);
+    }
+}
