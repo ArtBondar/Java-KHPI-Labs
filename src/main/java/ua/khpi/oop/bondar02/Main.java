e14419443afbc84318ddc837d4e42544b1968fa2
ArtBondar <mr.pika.2017@gmail.com>
2022-10-14 13:10:49 +0300

    Finish lab02

--- src/main/java/ua/khpi/oop/bondar02/Main.java
+++ src/main/java/ua/khpi/oop/bondar02/Main.java
@@ -3,9 +3,20 @@ package ua.khpi.oop.bondar02;
 import java.util.Random;
 
 public class Main {
-
+    private static int GetRandomNumber() {
+        return new Random().nextInt(999999);
+    }
+    private static int GetSumInNumber(int number){
+        int res = 0;
+        while(number > 0){
+            res += number % 10;
+            number /= 10;
+        }
+        return res;
+    }
     public static void main(String args[]) {
-        int rand = new Random().nextInt(999999);
-        System.out.println("Random number: " + rand);
+        int randomNumber = GetRandomNumber();
+        System.out.println("Random number: " + randomNumber);
+        System.out.println("Result: "+ GetSumInNumber(randomNumber));
     }
 }
